b2ca64c37e1384f5b636100a7c24959c11c49cd8
Pedro Remedios <pedro.remedios@gmail.com>
2019-07-31 14:07:12 +0100

    Solved googleservice-info.plist drama

--- hooks/handle_google_services.js
+++ hooks/handle_google_services.js
@@ -68,7 +68,8 @@ function getGoogleServiceTargetDir(context) {
             }
         }
         case "ios":
-            return platformPath;
+            return context.opts.plugin.dir;
+            // return platformPath;
         default:
             return undefined;
     }
@@ -108,6 +109,7 @@ function copyGoogleServiceOnIos(sourceDir, targetDir) {
     try {
         var sourceFilePath = path.join(sourceDir, "GoogleService-Info.plist");
         var targetFilePath = path.join(targetDir, "GoogleService-Info.plist");
+        console.log("copyGoogleServiceOnIos: ", targetFilePath);
         fs.copyFileSync(sourceFilePath, targetFilePath);
         return true;
     } catch (error) {
@@ -123,7 +125,7 @@ module.exports = function(context) {
 
         var prefZipFilename = "google-services";
         var zipFile = getZipFile(configPath, prefZipFilename);
-
+        console.log("zipFile: ", zipFile);
         // if zip file is present, lets unzip it!
         if (!zipFile) {
             return reject(
