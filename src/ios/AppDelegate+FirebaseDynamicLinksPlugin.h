2a86ac2343908ac8417096be4e1851895109cf53
Maksim Chemerisuk <chemerisuk@gmail.com>
2019-11-04 15:06:15 +0300

    update code, fixes #3

--- src/ios/AppDelegate+FirebaseDynamicLinksPlugin.h
+++ src/ios/AppDelegate+FirebaseDynamicLinksPlugin.h
@@ -1,4 +1,8 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate (FirebaseDynamicLinksPlugin)
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *)options;
+- (BOOL)application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler;
+
 @end
