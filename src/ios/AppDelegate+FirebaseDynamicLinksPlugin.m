6e41164e6f81504d8d1a03ac9992c10598d09374
Athorcis <Athorcis@users.noreply.github.com>
2023-02-28 17:01:30 +0000

    prefixing swizzling logic to avoid conflicts

--- src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
+++ src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
@@ -8,15 +8,15 @@
     static dispatch_once_t onceToken;
 
     dispatch_once(&onceToken, ^{
-        [self swizzleMethod:@selector(application:openURL:options:)];
-        [self swizzleMethod:@selector(application:continueUserActivity:restorationHandler:)];
+        [self FirebaseDynamicLinksPlugin_swizzleMethod:@selector(application:openURL:options:)];
+        [self FirebaseDynamicLinksPlugin_swizzleMethod:@selector(application:continueUserActivity:restorationHandler:)];
     });
 }
 
-+ (void)swizzleMethod:(SEL)originalSelector {
++ (void)FirebaseDynamicLinksPlugin_swizzleMethod:(SEL)originalSelector {
     Class class = [self class];
     NSString *selectorString = NSStringFromSelector(originalSelector);
-    SEL newSelector = NSSelectorFromString([@"swizzled_" stringByAppendingString:selectorString]);
+    SEL newSelector = NSSelectorFromString([@"FirebaseDynamicLinksPlugin_" stringByAppendingString:selectorString]);
     SEL defaultSelector = NSSelectorFromString([@"default_" stringByAppendingString:selectorString]);
     Method originalMethod = class_getInstanceMethod(class, originalSelector);
     Method newMethod = class_getInstanceMethod(class, newSelector);
@@ -32,9 +32,9 @@
     return FALSE;
 }
 
-- (BOOL)swizzled_application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *)options {
+- (BOOL)FirebaseDynamicLinksPlugin_application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *)options {
     // always call original method implementation first
-    BOOL handled = [self swizzled_application:app openURL:url options:options];
+    BOOL handled = [self FirebaseDynamicLinksPlugin_application:app openURL:url options:options];
     FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
     // parse firebase dynamic link
     FIRDynamicLink *dynamicLink = [[FIRDynamicLinks dynamicLinks] dynamicLinkFromCustomSchemeURL:url];
@@ -49,9 +49,9 @@
     return FALSE;
 }
 
-- (BOOL)swizzled_application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+- (BOOL)FirebaseDynamicLinksPlugin_application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
     // always call original method implementation first
-    BOOL handled = [self swizzled_application:app continueUserActivity:userActivity restorationHandler:restorationHandler];
+    BOOL handled = [self FirebaseDynamicLinksPlugin_application:app continueUserActivity:userActivity restorationHandler:restorationHandler];
     FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
     // handle firebase dynamic link
     return [[FIRDynamicLinks dynamicLinks]
